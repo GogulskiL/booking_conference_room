0eb16e3c69abc610e0c9add95188403d6d0e5104
łukasz gogulski <lukaszgogul@gmail.com>
2022-01-15 22:43:11 +0100

    first view

--- booking/booking/urls.py
+++ booking/booking/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from booking_app.views import HomePage
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', HomePage.as_view(), name='home'),
 ]

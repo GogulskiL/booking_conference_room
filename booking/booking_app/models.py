eebf039df5a8738714d9d488e28a2fc2b41b39f3
łukasz gogulski <lukaszgogul@gmail.com>
2022-01-16 12:05:21 +0100

    config templates

--- booking/booking_app/models.py
+++ booking/booking_app/models.py
@@ -1,3 +1,7 @@
+from unicodedata import name
 from django.db import models
 
-# Create your models here.
+class ConferenceRoom(models.Model):
+    name = models.CharField(max_length=255)
+    capacity = models.PositiveIntegerField()
+    projector_availbity = models.BooleanField(default=False)
--- booking/booking_app/views.py
+++ booking/booking_app/views.py
@@ -1,6 +1,10 @@
+from django import template
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
 class HomePage(TemplateView):
     template_name = "__base__.html"
+
+class AddRoom(TemplateView):
+    template_name = "add_room.html"

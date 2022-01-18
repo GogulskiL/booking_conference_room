111afb6927f01dbaed18785d9a90963dcbbc141b
łukasz gogulski <lukaszgogul@gmail.com>
2022-01-18 19:26:08 +0100

    pep-8 correct

--- booking/booking_app/models.py
+++ booking/booking_app/models.py
@@ -1,6 +1,7 @@
 from unicodedata import name
 from django.db import models
 
+
 class ConferenceRoom(models.Model):
     name = models.CharField(max_length=255)
     capacity = models.PositiveIntegerField()
